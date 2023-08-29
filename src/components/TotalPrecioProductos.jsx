1bd6009dc0545f19f96ad951a7e6c32929992b9c
Franco Werner <ifrank44445@gmail.com>
2023-08-29 05:18:00 -0300

    Cambio nombres de archivos

---
+++ src/components/TotalPrecioProductos.jsx
@@ -0,0 +1,20 @@
+
+import { useListadoFinalProducto } from "../hooks/useListadoFinalProducto"
+import React from "react"
+import { separarNumerosConDecimales } from "../helper/separarNumerosConDecimales"
+import styles from "../styles/TotalProductos.module.css"
+import { Row } from "react-bootstrap"
+
+
+
+export const TotalPrecioProductos = React.memo(() => {
+
+    const { listadoFinal } = useListadoFinalProducto()
+    return (
+        <Row className={`mx-0 mt-3   ${styles.preciosTotales}`}>
+            <p className={`${styles.precio}   d-inline-block  text-end fs-5 overflow-hidden`}>
+                Total : $ {separarNumerosConDecimales(listadoFinal)}
+            </p>
+        </Row>
+    )
+})
