0d899fd19c92cc8340b51f443c29e7c2e69409be
pauletti <P@gmail.com>
2023-08-30 20:49:31 -0300

    PlantillaCobro bug git

--- src/components/PlantillaCobro/PlantillaCobro.jsx
+++ src/components/PlantillaCobro/PlantillaCobro.jsx
@@ -14,34 +14,20 @@ export const PlantillaCobro = () => {
     return (
         <>
 
+
+
+
             <Container fluid className="p-0">
                 <ProductoSeleccionadoProvider>
-<<<<<<< HEAD
-
-                    <Container fluid className="overflow-hidden  d-flex p-0 flex-column">
-                        <Row>
-                            <Col className={`d-flex flex-column ${styles.contenedorCobroPrincipal}`}>
-                                <ContenedorCobroHeader />
-                                <ContenedorCobroBody />
-                            </Col>
-                        </Row>
-
-                        <Row className="flex-grow-1">
-                            <Col>
-                                <PlantillaPagos />
-                            </Col>
-                        </Row>
-                    </Container>
-=======
                     <Row >
                         <Col className={`d-flex flex-column ${styles.contenedorCobroPrincipal}`}>
                             <ContenedorCobroHeader />
                             <ContenedorCobroBody />
                         </Col>
                     </Row>
->>>>>>> werner
                 </ProductoSeleccionadoProvider>
             </Container>
+
         </>
     )
 }
