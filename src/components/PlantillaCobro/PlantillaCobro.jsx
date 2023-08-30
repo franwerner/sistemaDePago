651a137bfa7216f2f3fa5b813ccf5c1e50d7a406
pauletti <P@gmail.com>
2023-08-30 20:50:21 -0300

    a

--- src/components/PlantillaCobro/PlantillaCobro.jsx
+++ src/components/PlantillaCobro/PlantillaCobro.jsx
@@ -13,10 +13,6 @@ export const PlantillaCobro = () => {
 
     return (
         <>
-
-
-
-
             <Container fluid className="p-0">
                 <ProductoSeleccionadoProvider>
                     <Row >
