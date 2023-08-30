3deb6a4307004f8be0debc209dacea0f282199d4
pauletti <P@gmail.com>
2023-08-29 22:33:54 -0300

    arreglando PlantillaProductos definitivamente

--- src/components/PlantillaProductos/ContainerDeProductos.jsx
+++ src/components/PlantillaProductos/ContainerDeProductos.jsx
@@ -1,41 +1,17 @@
 import { Col, Container } from "react-bootstrap";
 import { ListaDeProductos } from "./ListaDeProductos";
 import styles from "../../styles/PlantillaProductos.module.css"
-import { useEffect, useState } from "react";
 
 export const ContainerDeProductos = ({ seccion }) => {
 
-    // const [maxHeight, setMaxHeight] = useState(850);
 
-    // const [click, setClick] = useState(false)
-
-    // useEffect(() => {
-
-    //     const navbarHeight = document.querySelector('.navbar-toggler')
-
-    //     const onClick = (e) => {
-
-    //         if (!click) {
-    //             setMaxHeight(maxHeight - 94);
-    //         }
-    //         else {
-    //             setMaxHeight(850)
-    //         }
-
-    //         setClick(prevClick => !prevClick);
-    //     }
-    //     navbarHeight.addEventListener("click", onClick)
-
-    //     return () => navbarHeight.removeEventListener("click", onClick)
-
-    // }, [click]);
-    
-   
     return (
         <>
-            <Container  className={` ${styles.contenedorProductos}  `}>
-                <Col className="flex-wrap my-2 flex-grow-1 border border-danger flex d-flex justify-content-center justify-content-md-start" >
+            <Container className={`pb-1 ${styles.contenedorProductos} flex-grow-1`}>
+                <Col style={{maxHeight : "0px"}} className={`p-0  flex-wrap d-flex justify-content-center justify-content-md-start`} >
+
                     <ListaDeProductos seccion={seccion} />
+
                 </Col>
             </Container>
         </>
