acd0a0f65524af53adc6253b0fb3a122243a466e
Franco Werner <ifrank44445@gmail.com>
2023-08-31 08:52:42 -0300

    recudcer metodo

---
+++ src/hooks/useMetodoDePagoReducer.js
@@ -0,0 +1,24 @@
+import { useReducer } from "react";
+
+
+ 
+const reducer = (state,action) =>{
+
+    switch(action.type){
+         
+    }
+
+}
+
+
+
+
+export const useMetodoDePagoReducer = () => {
+
+    const [metodoDePago,dispatch] = useReducer(reducer,[])
+
+
+    
+
+
+};
